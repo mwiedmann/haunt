efd47e04a6b82d5a3ffc224233f4c03bb5458e7d
Mark Wiedmann <mrwieds@gmail.com>
2026-06-07 14:08:32 -0400

    feat: new level, treasure count check

--- tools/ldtk-convert.js
+++ tools/ldtk-convert.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const rawText = fs.readFileSync("haunt.ldtk");
 const d = JSON.parse(rawText);
 
+const treasureCheck = []
+
 const convertFlip = (f) =>
     f === 1 ? 0b100 : f === 2 ? 0b1000 : f === 3 ? 0b1100 : 0
 
@@ -11,6 +13,13 @@ const createLevelCode = (level) => {
   let mapbase = []
   level.layerInstances[0].gridTiles.forEach(tile => {
     floor.push(tile.t)
+    if (tile.t >= 12 && tile.t <= 47) {
+      if (treasureCheck[tile.t]) {
+        console.error(`On level ${level.identifier} Tile ${tile.t} at (${tile.px[0]/16}, ${tile.px[1]/16}) is a duplicate treasure tile!`)
+      } else {
+        treasureCheck[tile.t] = true
+      }
+    }
   })
 
   let tileCount=0
@@ -39,5 +48,11 @@ for (let i=0; i<d.levels.length; i++) {
     fs.writeFileSync(`build/L${levelNum.toString().padStart(2, '0')}FLOOR.BIN`, new Uint8Array([...data.floor]), "binary");
     fs.writeFileSync(`build/L${levelNum.toString().padStart(2, '0')}MAP.BIN`, new Uint8Array([...data.mapbase]), "binary");
     console.log(`Created Level ${level.identifier}`)
+  }
+}
+
+for (let i=12; i<=47; i++) {
+  if (!treasureCheck[i]) {
+    console.error(`Tile ${i} is a missing treasure tile!`)
   }
 }
